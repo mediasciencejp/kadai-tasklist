cdb74a567f02f47ae145ef5b128f0ab517e7a6b1
Shinichi.M <contact@mediascience.jp>
2019-04-29 11:10:23 +0000

    Procfile

--- app/Task.php
+++ app/Task.php
@@ -6,5 +6,5 @@ use Illuminate\Database\Eloquent\Model;
 
 class Task extends Model
 {
-    public $statuses_list = ["wating" => "未着手", "workng" => "着手", "completed" => "完了"]; 
+    public $statuses_list = ["wating" => "未着手", "working" => "着手", "completed" => "完了"]; 
 }
