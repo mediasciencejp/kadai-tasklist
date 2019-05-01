44bf2663fc50269ddafae0059e3b383ffdc2fbf5
Shinichi.M <contact@mediascience.jp>
2019-05-01 08:17:53 +0000

    user registration

--- routes/web.php
+++ routes/web.php
@@ -15,3 +15,7 @@ Route::get('/', function () {
     return view('welcome');
 });
 Route::resource('tasks', 'TasksController');
+
+// User signup
+Route::get('signup', 'Auth\RegisterController@showRegistrationForm')->name('signup.get');
+Route::post('signup', 'Auth\RegisterController@register')->name('signup.post');
